8dd27b672912b707f53971f95f79b8beed4a2954
Aleksei Neronov <aleksei.neronov@yahoo.com>
2017-01-02 14:03:13 +0100

    Initial Commit

---
+++ Pollster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pollster
+//
+//  Created by Aleksei Neronov on 02.01.17.
+//  Copyright © 2017 Aleksei Neronov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
